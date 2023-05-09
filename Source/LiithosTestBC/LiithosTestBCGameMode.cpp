c3d4adbe890281726271e2c68dc18b3599cb017e
brunocitoni <bruno.citoni1@gmail.com>
2023-05-09 10:57:53 +0200

    final commit

--- Source/LiithosTestBC/LiithosTestBCGameMode.cpp
+++ Source/LiithosTestBC/LiithosTestBCGameMode.cpp
@@ -7,7 +7,7 @@
 ALiithosTestBCGameMode::ALiithosTestBCGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/LiithosTestBrunoCitoni/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
